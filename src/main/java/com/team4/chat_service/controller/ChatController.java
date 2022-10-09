ce810d4d5913e44c18144e6a8bcac885cdcfd32e
Hoang Linh <hlinh.jobs@gmail.com>
2022-10-09 22:15:05 +1100

    build chat-service

--- src/main/java/com/team4/chat_service/controller/ChatController.java
+++ src/main/java/com/team4/chat_service/controller/ChatController.java
@@ -1,7 +1,24 @@
 package com.team4.chat_service.controller;
 
+import com.team4.chat_service.model.Chat;
+import com.team4.chat_service.service.ChatService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping(value = "/chat/{senderId}/{receiverId}")
+    public List<Chat> getChats(@PathVariable Long senderId, @PathVariable Long receiverId){
+        return chatService.findChatsBySenderIdAndReceiverId(senderId, receiverId);
+    }
 }
--- src/main/java/com/team4/chat_service/service/ChatService.java
+++ src/main/java/com/team4/chat_service/service/ChatService.java
@@ -1,6 +1,25 @@
 package com.team4.chat_service.service;
 
 
+import com.team4.chat_service.model.Chat;
+import com.team4.chat_service.repo.ChatRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ChatService {
+    private final ChatRepository chatRepository;
+
+    public ChatService(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    public List<Chat> findChatsBySenderIdAndReceiverId(Long senderId, Long receiverId){
+        return chatRepository.findChatsBySenderIdAndReceiverId(senderId, receiverId);
+    }
+
+
+
 
 }
